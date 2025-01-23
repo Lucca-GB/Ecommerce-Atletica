fde53805770ed412f247d6e089d9e8d1233f4589
Lucca GB <luccag.barreto@gmail.com>
2025-01-23 19:45:55 -0300

    test first commit

--- Ecommerce-Atlética/obj/Debug/net8.0/Ecommerce-Atlética.AssemblyInfo.cs
+++ Ecommerce-Atlética/obj/Debug/net8.0/Ecommerce-Atlética.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce-Atlética")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76b8c54445fc2206ad78224ca4e0d373d12391ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8221cd117dbf2a3bccaa5e380ce42d4b44d8dccb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce-Atlética")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce-Atlética")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
